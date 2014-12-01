1721a472063e76f2ebbc4ac812dcb0a0635dc9d9
qiwsir <qiwsir@gmail.com>
2014-12-01 23:03:50 +0800

    query

--- handler/query.py
+++ handler/query.py
@@ -16,6 +16,34 @@ class QueryGene(tornado.web.RequestHandler):
         genechr = self.get_argument("genechr")
         vartype = self.get_argument("vartype")
         generef = self.get_argument("generef")
-#根据值，组装查询条件，即向字典中插入key:value
-        pos_find = db.find({"pos":{"$gte":int(pos_start), "$lte":int(pos_end)}})
-        self.render("query.html", result=pos_find)
+        
+        query_con = {}
+        
+        if pos_start and pos_end:
+            pos_con = {"$gte":int(pos_start), "$lte":int(pos_end)}
+            query_con["pos"] = pos_con
+        elif pos_start:
+            query_con["pos"] = int(pos_start)
+        elif pos_end:
+            query_con["pos"] = int(pos_end)
+        else:
+            pass
+
+        if genechr:
+            query_con["chr"] = genechr
+
+        if vartype:
+            query_con["vartype"] = vartype
+
+        if generef:
+            query_con["ref"] = generef
+
+        if query_con:
+            que = db.find(query_con)
+            #print list(que)
+            if que.count():
+                self.render("query.html", result=que)
+            else:
+                self.render("query.html", result=False)
+        else:
+            self.render("query.html", result=False)
